03def5b6e783e6a1167742a4ac62fdee778d1d72
Ivans Smirnovs <ivans@MacNovel.local>
2023-01-14 01:27:52 +0400

    Initial commit

---
+++ src/main/java/net/turtlethefish/regularchef/RegularChef.java
@@ -0,0 +1,16 @@
+package net.turtlethefish.regularchef;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegularChef implements ModInitializer {
+
+	public static final String MOD_ID = "regularchef";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/turtlethefish/regularchef/RegularChefClient.java
@@ -0,0 +1,10 @@
+package net.turtlethefish.regularchef;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegularChefClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
